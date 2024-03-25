119ab67adbb67f349978c9e2ea79c56a1a62c952
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-03-25 18:49:41 +0400

    Added State constructor

--- models/state.py
+++ models/state.py
@@ -12,3 +12,7 @@ class State(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
+    
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
