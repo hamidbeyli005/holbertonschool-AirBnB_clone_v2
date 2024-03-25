fd13d11fa6d2c1755d0c72b120298e13938d5825
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-03-25 18:38:31 +0400

    Added db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 
-from models.base_model import BaseModel, Base
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+import models
 
-class DBStorage(Base):
+
+class DBStorage:
     __engine = None
     __session = None
     
+
     def __init__(self):
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                                       format(os.getenv('HBNB_MYSQL_USER'),
@@ -17,41 +21,40 @@ class DBStorage(Base):
                                              pool_pre_ping=True))
         if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
-    
+
+        self.reload()
+
     def all(self, cls=None):
-        """ Returns a dictionary of models currently in storage """
-        from models import storage
-        from models.state import State
-        from models.city import City
-        from models.user import User
-        from models.place import Place
-        from models.review import Review
-        from models.amenity import Amenity
-        from models import classes
+        """ Returns a dictionary of objects """
         if cls:
-            objects = self.__session.query(classes[cls]).all()
+            if isinstance(cls, str):
+                cls = models.get(cls)
+            return {obj.__class__.__name__ + "." + obj.id: obj for obj in self.__session.query(cls)}
         else:
-            objects = []
-            for key, value in classes.items():
-                objects += self.__session.query(value).all()
-        return {obj.__class__.__name__ + '.' + obj.id: obj for obj in objects}
-    
+            obj_dict = {}
+            for cls in models.classes:
+                obj_dict.update({obj.__class__.__name__ + "." + obj.id: obj for obj in self.__session.query(cls)})
+            return obj_dict
+
     def new(self, obj):
         """ Adds new object to storage dictionary """
         self.__session.add(obj)
-    
+
     def save(self):
         """ Saves storage dictionary to file """
         self.__session.commit()
-    
+
     def delete(self, obj=None):
         """ Deletes obj from objects """
         if obj:
             self.__session.delete(obj)
-    
+
     def reload(self):
         """ Loads storage dictionary from file """
-        from sqlalchemy.orm import sessionmaker, scoped_session
         Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(Session)
+
+
+if __name__ == "__main__":
+    storage = DBStorage()
