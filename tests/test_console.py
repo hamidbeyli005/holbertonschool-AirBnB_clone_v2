8b68780c35cb31868f2f6afb017f78480aac31fc
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-03-28 12:17:05 +0400

    Adapted pycodestyle

--- tests/test_console.py
+++ tests/test_console.py
@@ -8,25 +8,22 @@ from unittest.mock import patch
 from io import StringIO
 
 
-
 class TestConsole(unittest.TestCase):
 
     def setUp(self):
         self.hbtn = HBNBCommand()
-    
+
     @patch('sys.stdout', new_callable=StringIO)
     def test_create_without_className(self, mock_stdout):
         self.hbtn.onecmd("create")
-        self.assertEqual(mock_stdout.getvalue(),"** class name missing **\n")    
-    
+        self.assertEqual(mock_stdout.getvalue(), "** class name missing **\n")
+
     @patch('sys.stdout', new_callable=StringIO)
     def test_create_invalid_class(self, mock_stdout):
         self.hbtn.onecmd("create InvaliClass")
-        self.assertEqual(mock_stdout.getvalue(),"** class doesn't exist **\n")
-        
-    @patch('sys.stdout',new_callable=StringIO)
+        self.assertEqual(mock_stdout.getvalue(), "** class doesn't exist **\n")
+
+    @patch('sys.stdout', new_callable=StringIO)
     def test_create_valid_class(self, mock_stdout):
         self.hbtn.onecmd("create State")
         self.assertTrue(mock_stdout.getvalue() != "")
-
-
