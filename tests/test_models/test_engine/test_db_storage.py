cdd85e700a5c6246b326c48a89bc69808fde3cd6
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-03-28 13:44:32 +0400

    Updated db_storage tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,66 +1,27 @@
-#!/usr/bin/python3
-""" """
 import unittest
 from unittest.mock import MagicMock, patch
 from models.engine import db_storage
 from models.state import State
-from os import getenv
-import MySQLdb
 
-
-
-class test_fileStorage(unittest.TestCase):
-    """ """
-
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
-                    "can't run if storage is file")
+class TestDBStorage(unittest.TestCase):
     def setUp(self):
         self.storage = db_storage.DBStorage()
-        if getenv("HBNB_TYPE_STORAGE") == "db":
-            self.db = MySQLdb.connect(getenv("HBNB_MYSQL_HOST"),
-                                    getenv("HBNB_MYSQL_USER"),
-                                    getenv("HBNB_MYSQL_PWD"),
-                                    getenv("HBNB_MYSQL_DB"))
-            self.cursor = self.db.cursor()
-
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
-                    "can't run if storage is file")
-    def tearDown(self):
-        """at the end of the test this will tear it down"""
-        if getenv("HBNB_TYPE_STORAGE") == "db":
-            self.db.close()
-        
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
-                    "can't run if storage is file")
-    def test_new_DBStorage(self):
-        """Tests for new() method"""
-        nb = self.cursor.execute("SELECT COUNT(*) FROM states")
-        state = State(name="Oregon")
-        state.save()
-        nb1 = self.cursor.execute("SELECT COUNT(*) FROM states")
-        self.assertEqual(nb1 - nb, 0)
 
     @patch.object(db_storage.DBStorage, '_DBStorage__session')
-    def test_delete_object(self, mock_session):
-        mock_obj = MagicMock()
-        self.storage.delete(mock_obj)
-        mock_session.delete.assert_called_once_with(mock_obj)
-    
-    @patch.object(db_storage.DBStorage, '_DBStorage__session')
-    def test_delete_none(self, mock_session):
-        self.storage.delete(None)
-        mock_session.delete.assert_not_called()
+    def test_all_with_class(self, mock_session):
+        mock_query = MagicMock()
+        mock_session.query.return_value = mock_query
+        self.storage.all(State)
+        mock_session.query.assert_called_once_with(State)
+        mock_query.all.assert_called_once()
 
     @patch.object(db_storage.DBStorage, '_DBStorage__session')
-    def test_save(self, mock_session):
-        self.storage.save()
-        mock_session.commit.assert_called_once()
-    
-    @patch.object(db_storage.DBStorage, '_DBStorage__session')
-    def test_new(self, mock_session):
-        mock_obj = MagicMock()
-        self.storage.new(mock_obj)
-        mock_session.add.assert_called_once_with(mock_obj)
+    def test_all_without_class(self, mock_session):
+        mock_query = MagicMock()
+        mock_session.query.return_value = mock_query
+        self.storage.all()
+        assert mock_session.query.call_count == len(db_storage.classes)
+        mock_query.all.assert_called()
 
 if __name__ == '__main__':
     unittest.main()
