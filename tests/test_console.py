b61faec76af8c50aeb4817e37c509b0c16d404f0
root <muradabbaszade6@gmail.com>
2024-03-31 12:44:51 +0000

    Update test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -13,6 +13,7 @@ from models.place import Place
 from models.review import Review
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
+from os import getenv
 
 
 class TestConsole(unittest.TestCase):
